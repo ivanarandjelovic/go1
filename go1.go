3fc3769d26f3299b7d152869943f6337cb12b172
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-01-26 12:28:48 +0100

    play with goroutines

--- go1.go
+++ go1.go
@@ -1,6 +1,29 @@
 package main
 
+import "runtime"
+import "fmt"
+
 func main() {
-	var x = "123"
-	println(x)
+	var x = runtime.GOMAXPROCS(0)
+	fmt.Println("Max procs = ", x)
+
+	var kanalica = make(chan string)
+	var kraj = make(chan bool)
+
+	go doSometime(kanalica, kraj)
+
+	for i := 65; i < 95; i++ {
+		println("mecem " + string(i))
+		kanalica <- string(i)
+	}
+	close(kanalica)
+
+	<-kraj
+}
+
+func doSometime(kanal chan string, kraj chan bool) {
+	for str := range kanal {
+		println(str)
+	}
+	kraj <- true
 }
