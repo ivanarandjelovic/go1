de7c8964f24209f7cab77eefadfd4825370ae4f9
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2018-01-26 12:45:40 +0100

    Structures ... first play

--- go1.go
+++ go1.go
@@ -1,12 +1,30 @@
 package main
 
-import "runtime"
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/ivanarandjelovic/go1/struct1"
+)
 
 func main() {
+
 	var x = runtime.GOMAXPROCS(0)
 	fmt.Println("Max procs = ", x)
 
+	//	goruts()
+
+	structs()
+}
+
+func doSometime(kanal chan string, kraj chan bool) {
+	for str := range kanal {
+		println(str)
+	}
+	kraj <- true
+}
+
+func goruts() {
 	var kanalica = make(chan string)
 	var kraj = make(chan bool)
 
@@ -21,9 +39,9 @@ func main() {
 	<-kraj
 }
 
-func doSometime(kanal chan string, kraj chan bool) {
-	for str := range kanal {
-		println(str)
-	}
-	kraj <- true
+// structs plays with structures
+func structs() {
+	s := struct1.NewStr1(1, "mile")
+
+	fmt.Println("Structure s is: ", s)
 }
---
+++ struct1/struct1.go
@@ -0,0 +1,21 @@
+// Package struct1 for playing arround with a structure
+//
+// Nothing important
+// just playing arround
+package struct1
+
+// Str1 is the structure we play with in this package
+// second line
+// third line (of comment for Str1)
+type Str1 struct {
+	id   int
+	name string
+}
+
+// NewStr1 constucts and returns new structure
+func NewStr1(id int, name string) *Str1 {
+	str := new(Str1)
+	str.id = id
+	str.name = name
+	return str
+}
